fa16bd140e38d39a6564020826a850c63112d8ad
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-08 16:18:25 -0300

    Subindo 46

--- Nivel5/Exercicio46.cs
+++ Nivel5/Exercicio46.cs
@@ -4,16 +4,28 @@ namespace Nivel5
     class Exercicio46
     {
         //Teste
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
-            int toReceive= 0;
-            int[] numbers = new int[5];
-            for (int i = 0; i <= 5; i++)
+            int vectorSize
+;
+            int resultado = 0;
+
+            Console.WriteLine("Digite a quantidade de números que você deseja adicionar no vetor: ");
+            vectorSize = Convert.ToInt32(Console.ReadLine());
+            int[] x = new int[vectorSize];
+            int[] y = new int[vectorSize];
+
+            for (int i = 0; i < vectorSize; i++)
             {
-                Console.WriteLine("Digite {0}º valor",i);
-                Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite um número para o primeiro vetor: ");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite um número para o segundo vetor: ");
+                y[i] = Convert.ToInt32(Console.ReadLine());
+
+                resultado += x[i] * y[i];
             }
-            Console.WriteLine(numbers);
+
+            Console.WriteLine($"\nProduto escalar = {resultado}");
         }
     }
 }
